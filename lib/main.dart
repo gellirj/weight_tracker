15bf80ac88ed9bdf4bc6232590db0b330a1d6d51
Randy Ellis <gellirj@gmail.com>
2019-06-14 00:26:55 -0500

    initial from https://github.com/MSzalek-Mobile/weight_tracker

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weight_tracker/home_page.dart';
+
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Weight Tracker',
+      theme: new ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: new HomePage(title: 'Weight Tracker'),
+    );
+  }
+}
